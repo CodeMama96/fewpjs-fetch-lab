3402ff1e989b3f990d75953fc108d13d3bfae58c
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-02 20:52:57 -0500

    commit tree b269425d654737ec67b4fb34e4c95cece235376b

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-function fetchBooks() {
 
-}
+function getPosts(){
+  
+  return
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json)); 
+  }
+  
 
-function renderBooks(books) {
-  const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
-}
+  function renderBooks(json) {
+    const main = document.querySelector('main')
 
-document.addEventListener('DOMContentLoaded', function() {
+    json.forEach(book => {
+      const h2 = document.createElement('h2')
+      h2.innerHTML = `<h2>${book.name}</h2>`
+      main.appendChild(h2)
+    })
+  }
+  document.addEventListener('DOMContentLoaded', function() {
+    console.log("I am here")
   fetchBooks()
-})
+  })
